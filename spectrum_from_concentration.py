88fc4b1157e0081ad418de4283c0572561c75a03
OlavRG <olavgrouwstra@gmail.com>
2014-10-24 17:28:45 +0200

    Start of spectrum to concentrations in python

---
+++ spectrum_from_concentration.py
@@ -0,0 +1,11 @@
+# Concentration analysis
+# Input
+    #G0S1.txt-G2S2.txt
+    #wavenumber.txt
+    #Compounds
+    #Directory of standard compounds
+
+interaction_length = 54.36
+compound_path   =   'L:\IST\OP\scratch\Adonis\Databases\PNNL Database\Compounds\\'
+file_regexp     =   '.*_25T?.TXT'
+file_extension  =   '.TXT'
