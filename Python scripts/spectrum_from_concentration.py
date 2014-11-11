ea302a9286b3d96e5af3a1b43745711e8c6ab990
OlavRG <olavgrouwstra@gmail.com>
2014-11-11 17:38:55 +0100

    Got regex for setting the cwd fixed.

--- Python scripts/spectrum_from_concentration.py
+++ Python scripts/spectrum_from_concentration.py
@@ -14,10 +14,22 @@
 
 import os
 import pandas as pd
+import re
+from lmfit import minimize, Parameters
 
-if os.path.dirname(os.path.abspath('__file__')) == os.getcwd()
-dir = os.path.dirname(os.path.dirname(os.path.abspath('__file__')))
-os.chdir(dir)
+sep = os.sep
+MAIN_PATH_NAME = 'Breath Analysis'
+STR_PATTERN = '.*'+sep+MAIN_PATH_NAME+sep
+MAIN_PATTERN = re.compile(STR_PATTERN.encode('string-escape')) #'.*'+sep+MAIN_PATH_NAME)
+
+if  re.match(MAIN_PATTERN,os.getcwd()):
+    #os.path.basename(os.getcwd()) == 'Python scripts':
+    dir = os.path.dirname(os.path.dirname(os.path.abspath('__file__')))
+    os.chdir(dir)
+elif os.path.basename(os.getcwd()) == 'Breath Analysis':
+    dir = os.path.dirname(os.path.abspath('__file__'))
+else:
+    sys.exit("CWD needs to be ~\Breath Analysis\ or at most one folder deeper")
 
 interaction_length = 54.36
 compound_path   =   'L:\IST\OP\scratch\Adonis\Databases\PNNL Database\Compounds\\'
@@ -28,3 +40,21 @@ file = 'Measurements\data of 26-8-2014\data no zeros\G0S1.txt'
 filename = os.path.join(dir, file)
 df = pd.read_csv(filename, sep='\t', header=1).astype(float)
 
+
+def residual(params, x, data, eps_data):
+    amp = params['amp'].value
+    pshift = params['phase'].value
+    freq = params['frequency'].value
+    decay = params['decay'].value
+
+    model = amp * sin(x * freq  + pshift) * exp(-x*x*decay)
+
+    return (data-model)/eps_data
+
+params = Parameters()
+params.add('amp', value=10)
+params.add('decay', value=0.007)
+params.add('phase', value=0.2)
+params.add('frequency', value=3.0)
+
+out = minimize(residual, params, args=(x, data, eps_data))
