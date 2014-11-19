4abc3769c8f4b049317f1e9114a8a8ba23787a36
OlavRG <olavgrouwstra@gmail.com>
2014-11-19 09:47:21 +0100

    Add three compounds, move strjoin and fixfig to matlab programs/others

--- Python scripts/spectrum_from_concentration.py
+++ Python scripts/spectrum_from_concentration.py
@@ -13,32 +13,62 @@
 #        - Dump all with concentration<whatever based on single molecule fit
 
 import os
+import sys
 import pandas as pd
 import re
 from lmfit import minimize, Parameters
 
-sep = os.sep
-MAIN_PATH_NAME = 'Breath Analysis'
-STR_PATTERN = '.*'+sep+MAIN_PATH_NAME+sep
-MAIN_PATTERN = re.compile(STR_PATTERN.encode('string-escape')) #'.*'+sep+MAIN_PATH_NAME)
+def main():
+    # Set the cwd assuming cwd is 'Breath Analysis' or one folder deeper.
+    sep = os.sep
+    MAIN_PATH_NAME = 'Breath Analysis'
+    STR_PATTERN = '.*'+sep+MAIN_PATH_NAME+sep
+    MAIN_PATTERN = re.compile(STR_PATTERN.encode('string-escape'))
+    
+    if  re.match(MAIN_PATTERN,os.getcwd()):
+        #os.path.basename(os.getcwd()) == 'Python scripts':
+        cwd = os.path.dirname(os.path.dirname(os.path.abspath('__file__')))
+        os.chdir(cwd)
+    elif os.path.basename(os.getcwd()) == 'Breath Analysis':
+        cwd = os.path.dirname(os.path.abspath('__file__'))
+    else:
+        sys.exit(    "CWD needs to be ~\Breath Analysis\ or at most one folder "
+                    "deeper")
 
-if  re.match(MAIN_PATTERN,os.getcwd()):
-    #os.path.basename(os.getcwd()) == 'Python scripts':
-    dir = os.path.dirname(os.path.dirname(os.path.abspath('__file__')))
-    os.chdir(dir)
-elif os.path.basename(os.getcwd()) == 'Breath Analysis':
-    dir = os.path.dirname(os.path.abspath('__file__'))
-else:
-    sys.exit("CWD needs to be ~\Breath Analysis\ or at most one folder deeper")
+if __name__ == '__main__':
+    main()
+cwd=os.getcwd()
 
+# Load standard compounds
+wavelength = pd.read_csv(compound_file_path, sep='\t', header=0).astype(float)
 interaction_length = 54.36
-compound_path   =   'L:\IST\OP\scratch\Adonis\Databases\PNNL Database\Compounds\\'
-file_regexp     =   '.*_25T?.TXT'
-file_extension  =   '.TXT'
+compound_all_folder   =   '\\Compounds\\'
+file_regexp     =   re.compile('.*_25T?.TXT').search
+compound_all_list = os.listdir(cwd+compound_all_folder)
+for ii in compound_all_list:
+    compound_folder = cwd+compound_all_folder+ii
+    compound_file_list = os.listdir(compound_folder)
+    for l in compound_file_list:
+        if file_regexp(l):
+            compound_file = file_regexp(l).group(0)
+            compound_file_path= os.path.join(compound_folder, compound_file)
+            G0S1 = pd.read_csv(compound_file_path, sep='\t', header=0).astype(float)
+        #for m in (file_regexp(l),):
 
+        #    print m
+            #if m:
+            #    print m.group(0)   
+ #   compound_path = 5
+  #  temporary_compound = pd.read_csv(filename, \
+#                                    sep='\t', header=1).astype(float)
+
+# Load measurements
 file = 'Measurements\data of 26-8-2014\data no zeros\G0S1.txt'
-filename = os.path.join(dir, file)
-df = pd.read_csv(filename, sep='\t', header=1).astype(float)
+filename = os.path.join(cwd, file)
+G0S1 = pd.read_csv(filename, sep='\t', header=1).astype(float)
+
+
+
 
 
 def residual(params, x, data, eps_data):
