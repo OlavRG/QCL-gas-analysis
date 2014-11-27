d6674d7c3c09a1e86896c160121673096ce91dd9
OlavRG <olavgrouwstra@gmail.com>
2014-11-27 17:41:01 +0100

    fix filtering script, works properly and can specify a wavenumber region for the filtering

--- Matlab programs/compound_filter.m
+++ Matlab programs/compound_filter.m
@@ -1,7 +1,7 @@
-% This program requires the folder of saved PNNL compounds as input, then
-% filters out the compounds that at do not reach minimum_absorbance at
-% single wavenumber. The absorbance of each compound is computed using an
-% assumed_concentration.
+% This program requires a cell list of compound names as they appear in
+% PNNL as input, then filters out the compounds that do not reach
+% minimum_absorbance at single wavenumber. The absorbance of each compound
+% is computed using an assumed_concentration.
 
 % Can use 'L:\IST\OP\scratch\Olav Grouwstra\Compounds\'; as testing
 % directory
@@ -22,8 +22,23 @@ addpath(genpath('H:\My Documents\GitHub\QCL-gas-analysis\Matlab programs'));
 % makes this stricter.
     minimum_absorbance  =   10^-7; % in base-10 absorbance: I=I0*10^(-A)
     assumed_concentration = 0.001; % in ppmv
-    INTERACTION_LENGTH = 1; % in meter
+    interaction_length = 1; % in meter
 
+% In case one wishes to filter in a certain wavelength range
+%     wavelength_start    = 1020;
+%     wavelength_end      = 1100;
+     if exist('wavelength_start','var')
+         [wavelength_start_index,~]  =   find(wavenumber==wavelength_start);
+     else
+         wavelength_start_index = 1;
+     end
+     
+     if exist('wavelength_end','var')
+         [wavelength_end_index,~]    =   find(wavenumber==wavelength_end);
+     else
+         wavelength_end_index = length(wavenumber);
+     end
+     
 %     compoundPathFileList      =   cellstr(ls(STANDARD_COMPOUND_PATH));
 %     compoundPathFileList      =   compoundPathFileList(3:end,1);
     load('H:\My Documents\GitHub\QCL-gas-analysis\Breath molecules_2.mat')
@@ -31,7 +46,7 @@ addpath(genpath('H:\My Documents\GitHub\QCL-gas-analysis\Matlab programs'));
 
     
     standardCompoundDir  =   cell(length(compoundPathFileList),1);
-    database_compounds_all   =   zeros(length(wavenumber),length(compoundPathFileList));
+    absorptivity_all_compounds   =   zeros(length(wavenumber),length(compoundPathFileList));
     nStandardCompound=0;
     nEmpty1=0;
     indexEmptyFolder1 = zeros(length(compoundPathFileList),1);
@@ -48,41 +63,41 @@ addpath(genpath('H:\My Documents\GitHub\QCL-gas-analysis\Matlab programs'));
             indexEmptyFolder1(nEmpty1)     =   k;
         else
             nStandardCompound=nStandardCompound+1;
-            [ database_compounds_all(:,nStandardCompound) ] =   load_compound(wavenumber,...
+            [ absorptivity_all_compounds(:,nStandardCompound) ] =   load_compound(wavenumber,...
                 [STANDARD_COMPOUND_PATH compoundPathFileList{k}(1:end) '\' standardFileName]);
         end
     end
 
-    % Resize database_compounds_all to account for empty folders in 
-    % length(standardCompoundList) to which it was calibrated
-    database_compounds_all  =   database_compounds_all(:,1:nStandardCompound);
+    % Resize absorptivity_all_compounds to account for folders without a
+    % file matching the regex. 
+    absorptivity_all_compounds  =   absorptivity_all_compounds(:,1:nStandardCompound);
     standardCompoundList = compoundPathFileList;
     indexEmptyFolder1(indexEmptyFolder1==0) = [];
     if exist('indexEmptyFolder1','var') 
         standardCompoundList(indexEmptyFolder1)=[];
     end
-    [row_standard_compounds, nStandardCompound] = size(database_compounds_all);
+    [row_standard_compounds, nStandardCompound] = size(absorptivity_all_compounds);
     
-    database_compounds_all2 = database_compounds_all*INTERACTION_LENGTH*assumed_concentration;
-    database_compounds_all3 = zeros(row_standard_compounds,nStandardCompound);
+    database_compounds_all_absorbance = absorptivity_all_compounds*interaction_length*assumed_concentration;
+    absorptivity_all_compounds_filtered = zeros(row_standard_compounds,nStandardCompound);
     nPassedCompound = 0;
     nCutCompound = 0;
     indexEmptyFolder2 = zeros(length(standardCompoundList),1);
     for ii=1:nStandardCompound
-        if max(database_compounds_all2(:,ii)) >= minimum_absorbance
+        if max(database_compounds_all_absorbance(wavelength_start_index:wavelength_end_index,ii)) >= minimum_absorbance
             nPassedCompound = nPassedCompound + 1;
-            database_compounds_all3(:,nPassedCompound) = database_compounds_all2(:,ii);
+            absorptivity_all_compounds_filtered(:,nPassedCompound) = absorptivity_all_compounds(:,ii);
         else
             nCutCompound = nCutCompound + 1;
             indexEmptyFolder2(nCutCompound)     =   ii;
         end
     end
     
+    absorptivity_all_compounds_filtered = absorptivity_all_compounds_filtered(:,1:nPassedCompound);
     filteredCompoundList = standardCompoundList;
     indexEmptyFolder2(indexEmptyFolder2==0) = [];
     if exist('indexEmptyFolder2','var') 
         filteredCompoundList(indexEmptyFolder2)=[];
-        database_compounds_all3(:,indexEmptyFolder2)=[];
     end
-save compounds_twice_filtered.mat database_compounds_all3 filteredCompoundList
+% save compounds_twice_filtered.mat absorptivity_all_compounds_filtered filteredCompoundList wavenumber
     
