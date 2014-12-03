7707af8963b583438f6cd8160d551f13845bbf64
OlavRG <olavgrouwstra@gmail.com>
2014-12-03 15:57:43 +0100

    loading measurements complete

--- Python scripts/remove_CO2_get_concentrations.py
+++ Python scripts/remove_CO2_get_concentrations.py
@@ -2,12 +2,15 @@
 # 2. Subtract all absorbances except that of the CO2 molecule from the measurement.
 # 3. Fit Voigth peaks to CO2-only spectrum. Now subtract those from total spectrum
 
+# TODO:
+# - get script working
+# - use while loop to get to right directory in main()
+
 import os
 import sys
 import pandas as pd
 import re
 import numpy as np
-from lmfit import minimize, Parameters
 
 def main():
     # Set the cwd assuming cwd is 'Breath Analysis' or one folder deeper.
@@ -33,11 +36,17 @@ cwd=os.getcwd()
 
 # Get absorbance .txt input to dataframes
 # Load measurements
-breath_spectrum_path = 'Measurements\data of 26-8-2014\data no zeros\\'
-breath_spectrum_list = os.listdir(breath_spectrum_path)
-for ii in breath_spectrum_list:
-    filename = os.path.join(cwd, breath_spectrum_path, ii)
-    breath_spectrum = pd.read_csv(filename, sep='\t', header=1).astype(float)
+breath_spectrum = []
+breath_spectrum_folder = 'Measurements\data of 26-8-2014\data no zeros\\'
+breath_spectrum_file_list = os.listdir(breath_spectrum_folder)
+for ii in breath_spectrum_file_list:
+    breath_spectrum_file_path = os.path.join(cwd,breath_spectrum_folder,ii)
+    breath_spectrum_temp = pd.read_table(breath_spectrum_file_path).astype(float).T # Later append requires transpose
+    breath_spectrum.append(breath_spectrum_temp)
+
+healthy = breath_spectrum[0].append(breath_spectrum[1]).T # transpose back to samples in columns
+asthma = breath_spectrum[4].append(breath_spectrum[5]).T
+
 
 
 # Get database absorbance .txt input to dataframes
